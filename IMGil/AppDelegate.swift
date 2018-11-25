2a61376e45d67b91364def1332dd5f127610877b
byeonggil.park <sky923@gmail.com>
2018-11-25 17:49:56 +0900

    CHG: 프로젝트 이름 변경. kim -> Input Method Gil

--- IMGil/AppDelegate.swift
+++ IMGil/AppDelegate.swift
@@ -1,21 +1,26 @@
 //
 //  AppDelegate.swift
-//  kim
+//  Input Method Gil
 //
-//  Created by gil on 11/25/18.
+//  Created by gil, sky923@gmail.com on 11/25/18.
 //  Copyright © 2018 sky923. All rights reserved.
 //
 
 import Cocoa
+import InputMethodKit
+
+private var server: IMKServer?
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
 	@IBOutlet weak var window: NSWindow!
 
-
 	func applicationDidFinishLaunching(_ aNotification: Notification) {
-		// Insert code here to initialize your application
+		NSLog("IMGil(Input Method Gil) Launching...")
+		let bundle = Bundle.main
+		server = IMKServer(name: "com.sky923.inputmethod.gil",
+						   bundleIdentifier: bundle.bundleIdentifier)
 	}
 
 	func applicationWillTerminate(_ aNotification: Notification) {
---
+++ IMGil/IMGilController.swift
@@ -0,0 +1,14 @@
+//
+//  IMGilController.swift
+//  Input Method Gil
+//
+//  Created by gil, sky923@gmail.com on 11/25/18.
+//  Copyright © 2018 sky923. All rights reserved.
+//
+
+import Foundation
+import InputMethodKit
+
+@objc(IMGilController)
+internal class IMGilController : IMKInputController {
+}
